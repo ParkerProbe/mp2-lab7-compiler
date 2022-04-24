653c31d85a03a02d5cdb3f3f7604f3f347ebd313
ParkerProbe <kolessnikoff.d.e@gmail.com>
2022-04-24 10:57:59 +0300

    Denis Add Files

--- base/main.cpp
+++ base/main.cpp
@@ -2,13 +2,14 @@
 #include "vector"
 #include "string"
 #include <fstream>
+#include <new>
 #include <sys/types.h>
 #include "string.h"
-#include "compiler_data.h"
+#include "compiler_config.h"
+#include "interface.h"
+
+
 #define COMPILER_VERSION 0.1
-
-
-
 
 
 enum  return_codes
@@ -27,112 +28,99 @@ enum  return_codes
 
 int main(int argc, char* argv[])
 {
-    /*        
-    
-        Compiler parameters
-    
-    */
-    compiler_parameters conf; 
+    compiler_parameters compl_conf; 
 
-    std::ifstream* source_code = nullptr;
-    if (!source_code) {
-        return k_MEMORY_ALLOC_ERROR;
-    }
+    try {
+        
+        std::ifstream* source_code = nullptr;
+        
+        Interface interface;
+        // ErrorHandler* err = new ErrorHandler;
+        
+        // TextLinker* txt_link = new TextLinker;
 
-    // ErrorHandler* err = new ErrorHandler;
-    // if (!err) {
-    //     return k_MEMORY_ALLOC_ERROR;
-    // }
-    
-    // TextLinker* txt_link = new TextLinker;
+        /* 
+        
+            Argument Analysis 
+        
+        */ 
 
-    // Argument analysis
+        source_code = new std::ifstream(argv[1]);
+        for (int i = 2; i < argc; i++) {
+            if (strcmp(argv[i] , "-l") == 0) {
+                compl_conf.lexer_only = true; 
+            }
+            if (strcmp(argv[i] , "-s") == 0) {
+                compl_conf.syntax_only = true; 
+            }
+            if (strcmp(argv[i] , "-g") == 0) {
+                compl_conf.gen_only = true; 
+            }
+            if (strcmp(argv[i] , "-v") == 0) {
+                compl_conf.pr_version = true;
+            }
+            if (strcmp(argv[i] , "--help") == 0) {
+                compl_conf.pr_help = true;
+            }
+            
+        }
 
-    source_code = new std::ifstream(argv[1]);
-    for (int i = 2; i < argc; i++) {
-        if (strcmp(argv[i] , "-l") == 0) {
-           conf.lexer_only = true; 
-        }
-        if (strcmp(argv[i] , "-s") == 0) {
-           conf.syntax_only = true; 
-        }
-        if (strcmp(argv[i] , "-g") == 0) {
-           conf.gen_only = true; 
-        }
-        if (strcmp(argv[i] , "-v") == 0) {
-            conf.pr_version = true;
-        }
-        if (strcmp(argv[i] , "--help") == 0) {
-            conf.pr_help = true;
+        if (source_code == nullptr) {
+            compl_conf.pr_path_dlg = true;
+            interface.print_file_dialog(source_code);
         }
         
+        if (source_code->fail()) {
+            return k_SOURCE_FILE_ERROR;
+        }
+
+        /* 
+        
+            Start Lexical Analysis 
+        
+        */
+
+        // LexicalAnalyzer* lex_analyzer = new LexicalAnalyzer
+        // (
+        //     source_code,
+        //     err,
+        //     lexer_only
+        // );
+        // if(lexer_only) {}
+
+                
+        /* 
+            
+            Start Syntax Analysis 
+
+        */
+
+        // SyntaxAnalyzer* syn_analyzer = new SyntaxAnalyzer
+        // (
+        //     txt_link,
+        //     err,
+        //     syntax_only
+        // );
+
+        // if(syntax_only) {}
+
+        /* 
+            Start Interpretation
+
+        */
+
+        // Interpretator* intpr = new Interpretator
+        // (
+        //     txt_link,
+        //     err,
+        //     gen_only
+        // );
+
+        source_code->close();
     }
-    if (source_code == nullptr) {
-        return k_NO_ERROR;
+    catch(std::bad_alloc& e) {
+        return k_MEMORY_ALLOC_ERROR;
     }
-    else if(source_code->fail()) {
-        // PRINT FATAL ERROR
-        return k_SOURCE_FILE_ERROR;
-    }
-
-    /* 
-        Start Lexical Analysis 
-
-    */
-    // LexicalAnalyzer* lex_analyzer = new LexicalAnalyzer
-    // (
-    //     source_code,
-    //     err,
-    //     lexer_only
-    // );
-    // Token* token = nullptr;
-    // Error Work
-    // if (!lex_analyzer) {
-    //     return k_MEMORY_ALLOC_ERROR;
-    // }
-
-    // if(lexer_only) {}
-
-    // 
-             
-    /* 
-        Start Syntax Analysis 
-
-    */
-    // SyntaxAnalyzer* syn_analyzer = new SyntaxAnalyzer
-    // (
-    //     lex_analyzer,
-    //     txt_link,
-    //     err,
-    //     syntax_only
-    // );
-    // // Error Work
-    // if (!syn_analyzer) {
-    //     return k_MEMORY_ALLOC_ERROR;
-    // }
-    //
-
-    // if(syntax_only) {}
-
-
-
-    /* 
-        Start Interpretation
-
-    */
-    // Interpretator* intpr = new Interpretator
-    // (
-    //     txt_link,
-    //     err,
-    //     gen_only
-    // );
-    // // Error Work
-    // if (!intpr) {
-    //     return k_MEMORY_ALLOC_ERROR;
-    // }
-    //
-
-    source_code->close();
 
 
     return 0;

160725d47bac536f59bdf295e590e0f0c87215d8
ParkerProbe <kolessnikoff.d.e@gmail.com>
2022-05-25 10:04:44 +0300

    Denis Upd Test

--- base/test_files/test.pas
+++ base/test_files/test.pas
@@ -1,26 +1,26 @@
 program Example;
 const
-  c1: double = -7,24423;
+  c1: double = -7.24423;
   c2: integer = -2;
   c3: integer = 1;
 var
   num1: integer = 2;
   num2: double = 0.12;
-  num3: double = -7,24423;
+  nUm3: double = -7.24423;
   NUM4: integer;
 begin { Commentary}
   num4 := 2; 
   num1 := 5;
   num2 := 20;
   write('Введите четное целое число');
-  Read(num1);
+  read(num1);
   write('Введите вещественное число');
-  Read(num3);
+  read(num3);
   if num1 mod 2 = 0 then
   begin
-    if c1 == 20 then
+    if c1 = 20 then
     begin
-      num3 := (num1-num2*5 div 2)/(d*2);
+      num3 := (num1-c2*5 div num1)/(c1*2);
       write('Result = ', num1);
     end
     else
@@ -30,16 +30,17 @@ begin { Commentary}
   else
   begin
     write('2');
-    Read(num3);
-    if num3 == 20 then
+    read(num3);
+    if num3 = 20.0 then
     begin
-      num2:=(num1-num2*5 div 2)/(d*2);
+      num2:=(num1-num4*5 mod 2)/(c1*2);
       write('3 = ', num2);
     end
     else
-      if c1 == 10 then
-        if num2 == 30 then
+      if c1 = 10 then
+        if num2 = 30 then
           write('4')
   end
 end.
 {Program end}
+{ In  pascal mod, div for integer only}
